7bc87814b58650c6310354f5f3e86a820050a719
fbrunopereira <fbrunop@gmail.com>
2026-05-19 12:51:27 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,23 @@
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
+const CACHE_NAME = "pipeline-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('activate', (event) => {
-  return self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  // deixa passar tudo normalmente
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
